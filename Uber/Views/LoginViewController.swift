becb15c63f5f0968a7bc1b6ab64b8290174b14c2
OybekNarzikulov <oybeknarzikulov1999@gmail.com>
2022-11-02 18:43:01 +0500

    Project Setup

---
+++ Uber/Views/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Uber
+//
+//  Created by Oybek Narzikulov on 02/11/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+
+}
